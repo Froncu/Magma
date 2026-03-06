7b33f84cb83bd977cfccebff55861b8d5ea063f0
Jakub Fratczak <jakubfratczak204@gmail.com>
2026-03-06 18:43:35 +0100

    [CHORE] stop logging the program arguments

--- source/magma.cpp
+++ source/magma.cpp
@@ -10,11 +10,8 @@ namespace eru
 
 namespace mgm
 {
-   Magma::Magma(std::span<char const* const> const arguments)
+   Magma::Magma(std::span<char const* const> const)
    {
-      for (auto&& argument : arguments)
-         eru::Locator::get<eru::Logger>().info(argument);
-
       window_.change_extent({ 1280, 720 });
       window_.change_position({ SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED });
       window_.change_title("Magma");
